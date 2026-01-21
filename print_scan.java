a63efba553f3bc225c7a555188aecbebf3063383
Shiva Mani <mop91838@gmail.com>
2026-01-21 10:17:36 +0000

    update java

---
+++ print_scan.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class print_scan {
+    public static void main (String[] args){
+
+
+
+
+
+
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number:");
+        int n = sc.nextInt();
+        System.err.println("");
+        System.out.print(n);
+
+
+    }
+    
+}
